9e23b15d82b968ca628698d0c6a7b906d214ef94
Charles GAYDON <charles.gaydon@gmail.com>
2021-06-29 11:18:54 +0200

    Complete docstrings in prepare_data.py

--- utils/prepare_data.py
+++ utils/prepare_data.py
@@ -12,8 +12,6 @@ import shapefile
 import pandas as pd
 from tqdm import tqdm
 
-# from config import args
-
 
 parser = ArgumentParser(description="model")  # Byte-compiled / optimized / DLL files
 parser.add_argument(
@@ -28,10 +26,14 @@ parser.add_argument(
     type=str,
     help="Path to folder of las files to copy from.",
 )
-args, _ = parser.parse_known_args()
+preparation_args, _ = parser.parse_known_args()
 
 
 def get_dataframe_from_shapefile(shapefile_path):
+    """
+    From a .dbd of a shapefile, create a pd.DataFrame.
+    Column NOM is the ID of the entities of the shapefile.
+    """
     sf = shapefile.Reader(shapefile_path)
     records = {rec.ID: rec.as_dict() for rec in sf.records()}
     df = (
@@ -48,7 +50,7 @@ def gather_las_data_from_shapefile_selection(
     to an output folder.
     """
     parcel_names_in_shapefile = get_dataframe_from_shapefile(
-        args.parcel_shapefile_path
+        preparation_args.parcel_shapefile_path
     ).NOM.values
     las_files = [f for f in get_files_of_type_in_folder(las_folder_path, ".las")]
     las_files_subset = [
@@ -66,12 +68,15 @@ def gather_las_data_from_shapefile_selection(
 
 def main():
     # Turn the shapefile into a csv for easier exploration.
-    df = get_dataframe_from_shapefile(args.parcel_shapefile_path)
-    df.to_csv(args.parcel_shapefile_path.replace(".shp", ".csv"), index=False)
+    df = get_dataframe_from_shapefile(preparation_args.parcel_shapefile_path)
+    df.to_csv(
+        preparation_args.parcel_shapefile_path.replace(".shp", ".csv"), index=False
+    )
+    # List entities in shapefile and move corresponding las files from their storage folder to the folder containing the shapefile
     gather_las_data_from_shapefile_selection(
-        args.las_folder_path,
-        args.parcel_shapefile_path,
-        os.path.dirname(args.parcel_shapefile_path),
+        preparation_args.las_folder_path,
+        preparation_args.parcel_shapefile_path,
+        os.path.dirname(preparation_args.parcel_shapefile_path),
     )
 
 
