39099410e1134de1f2fbbc02ca004cb66561c563
Charles GAYDON <charles.gaydon@gmail.com>
2021-08-13 18:08:44 +0200

    DATA: add synthetic ground points to deal with occlusion for low veg

--- data_loader/loader.py
+++ data_loader/loader.py
@@ -5,6 +5,7 @@ import torchnet as tnt
 import functools
 from collections import namedtuple
 import copy
+from inference.geotiff_raster import get_x_y_meshgrid
 
 
 def get_train_val_datasets(dataset, args, train_idx=None, val_idx=None):
